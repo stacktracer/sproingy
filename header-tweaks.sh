a9fccaca635b0781d8ec37487f38d1ef7dcc988d
Mike Hogye <stacktracer@gmail.com>
2021-05-22 20:47:39 -0400

    Fix error in header-tweaks script

--- header-tweaks.sh
+++ header-tweaks.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 scriptDir=$(readlink -nf "$(dirname "$0")")
 cd "$scriptDir"
 
+mkdir -p "./include/"
 rsync -rI --delete "/usr/include/glib-2.0/" "./include/glib-2.0/"
 rsync -rI --delete "/usr/include/gtk-3.0/" "./include/gtk-3.0/"
 patch -p0 < "./header-tweaks.patch"
