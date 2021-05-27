0da13f58b367144807d7da485168c88f6dc72f8b
Mike Hogye <stacktracer@gmail.com>
2021-05-26 23:21:20 -0400

    Add a misc/ subdir, and move several top-level files into it

--- header-tweaks.sh
+++ header-tweaks.sh
@@ -7,4 +7,4 @@ cd "$scriptDir"
 
 mkdir -p "./include"
 rsync -rI --delete "/usr/include/glib-2.0/" "/usr/include/gtk-3.0/" "./include/"
-patch -p0 < "./header-tweaks.patch"
+patch -p0 < "./misc/header-tweaks.patch"
